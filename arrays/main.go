34139537568c4d2574fa842d1114440dac384a4b
iris314 <irisqaz@gmail.com>
2020-01-07 17:36:31 -0800

    arrays

--- arrays/main.go
+++ arrays/main.go
@@ -6,7 +6,8 @@ type mySlice []int
 
 func main() {
 	var arr mySlice = make(mySlice, 3, 10)
-	//arr = append(arr, 8)
+	arr[0] = 7
 	fmt.Printf("type: %T value: %v length: %d capacity: %d", arr, arr, len(arr), cap(arr))
 	fmt.Println()
+	fmt.Println(arr[0])
 }
