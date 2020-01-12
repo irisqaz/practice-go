7cee0447c873a440925c84bcffca67bc92a2f722
iris314 <irisqaz@gmail.com>
2020-01-12 15:34:16 -0800

    strings

---
+++ strings/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	ints := []byte{72, 101, 108, 108, 111, 33}
+
+	fmt.Println()
+	fmt.Printf("%v %s", ints, ints)
+	fmt.Println()
+
+	ints[0] = 73
+
+	str := string(ints)
+
+	str = str + " world"
+
+	str = strings.ToUpper(str)
+
+	fmt.Println()
+	fmt.Printf("%v %s", str, str)
+	fmt.Println()
+
+	for i := 0; i < len(str); i++ {
+		fmt.Println(str[i])
+	}
+
+	for _, chr := range str {
+		fmt.Printf("%c \n", chr)
+	}
+
+}
