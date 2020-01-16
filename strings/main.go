8e3fe7f01494b9cbf713e03cf46f850605d5876d
iris314 <irisqaz@gmail.com>
2020-01-15 21:23:13 -0800

    slices

--- strings/main.go
+++ strings/main.go
@@ -2,35 +2,42 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
 
-	ints := []byte{72, 101, 108, 108, 111, 33}
+	/* 	ints := []byte{72, 101, 108, 108, 111, 33}
 
-	fmt.Println()
-	fmt.Printf("%v %s", ints, ints)
-	fmt.Println()
+	   	fmt.Println()
+	   	fmt.Printf("%v %s", ints, ints)
+	   	fmt.Println()
 
-	ints[0] = 73
+	   	ints[0] = 73
 
-	str := string(ints)
+	   	str := string(ints)
 
+	   	str = str + " world"
+
+	   	str = strings.ToUpper(str)
+
+	   	fmt.Println()
+	   	fmt.Printf("%v %s", str, str)
+	   	fmt.Println()
+
+	   	for i := 0; i < len(str); i++ {
+	   		fmt.Println(str[i])
+	   	}
+
+	   	for _, chr := range str {
+	   		fmt.Printf("%c \n", chr)
+	   	} */
+
+	str := "hello"
 	str = str + " world"
+	fmt.Println(str)
 
-	str = strings.ToUpper(str)
-
+	sub := str[2:]
+	fmt.Printf("%v", sub)
 	fmt.Println()
-	fmt.Printf("%v %s", str, str)
-	fmt.Println()
-
-	for i := 0; i < len(str); i++ {
-		fmt.Println(str[i])
-	}
-
-	for _, chr := range str {
-		fmt.Printf("%c \n", chr)
-	}
 
 }
