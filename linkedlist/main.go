5b50513abd2ddc2fd918d4f40c52fc44bee53169
iris314 <irisqaz@gmail.com>
2020-01-20 13:18:58 -0800

    simple linkedlist

--- linkedlist/main.go
+++ linkedlist/main.go
@@ -24,6 +24,25 @@ func (n *node) String() string {
 	return r + "]"
 }
 
+type list struct {
+	head *node
+	tail *node
+}
+
+func (l list) String() string {
+
+	r := "["
+
+	for curr := l.head; curr != nil; curr = curr.next {
+		r = r + strconv.Itoa(curr.data)
+		if curr.next != nil {
+			r = r + ", "
+		}
+	}
+
+	return r + "]"
+}
+
 func main() {
 	n := node{}
 	fmt.Println(&n)
@@ -34,10 +53,17 @@ func main() {
 	fmt.Println(&n)
 
 	var n2 *node
-	fmt.Println("Empty list n2: ", n2)
+	fmt.Println("Empty list n2:", n2)
 
 	n2 = &node{1, n2}
 	n2 = &node{2, n2}
 	n2 = &node{3, n2}
 	fmt.Println(n2)
+
+	var l list
+	fmt.Println("Empty list l:", l)
+	l.head = &node{10, l.head}
+	l.head = &node{11, l.head}
+	l.head = &node{12, l.head}
+	fmt.Println(l)
 }
